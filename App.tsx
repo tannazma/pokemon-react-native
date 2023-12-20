fc18b4620de4a3e60ccc1b4a6f7171668b934529
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-21 00:15:30 +0100

    changes app -> homescreen component and created stacks navigations

--- App.tsx
+++ App.tsx
@@ -1,47 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-import PokemonImage from "./components/PokemonImg";
-import { Pokemon } from "./types";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import PokemonDetailPage from "./components/PokemonDetailPage";
+import HomeScreen from "./components/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
-  useEffect(() => {
-    const getPokemons = async () => {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-      const data = await response.json();
-      setPokemons(data.results);
-    };
-    getPokemons();
-  }, []);
-
   return (
-    <View>
-      <Text style={styles.title}>Pokemons list</Text>
-      <ScrollView>
-        <View style={styles.container}>
-          {pokemons.map((pokemon) => (
-            <PokemonImage key={pokemon.name} pokemon={pokemon} />
-          ))}
-        </View>
-      </ScrollView>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen name="Pokemon" component={PokemonDetailPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    marginTop: 20,
-    display: "flex",
-    flexWrap: "wrap",
-    flexDirection: "row",
-  },
-  title: {
-    fontWeight: "800",
-    fontSize: 30,
-    marginTop: 40,
-  },
-});
