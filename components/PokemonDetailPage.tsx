fe495f82ab6810d8f518eb3d2014dfc3e54775ad
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-21 00:18:28 +0100

    created a test pokemon detail page component

---
+++ components/PokemonDetailPage.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const PokemonDetailPage = () => {
+  return (
+    <View>
+      <Text>Pokemon Detail page</Text>
+    </View>
+  );
+};
+export default PokemonDetailPage;
