f3bce5ca919be1d6a19cf632a00fbcac6b0f9d10
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-20 11:44:07 +0100

    use Pokemons api to map over the names results and show in front

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const data = await response.json();
+      setPokemons(data.results);
+    };
+    getPokemons();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Pokemons list</Text>
+      <ScrollView>
+        {pokemons.map((pokemon) => (
+          <Text key={pokemon.id}>{pokemon.name}</Text>
+        ))}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
