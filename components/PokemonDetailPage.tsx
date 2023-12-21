bac13a1c020ca683bd3379802d929ee93e8ba197
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-21 10:37:05 +0100

    created pokemon detail page with fetch

--- components/PokemonDetailPage.tsx
+++ components/PokemonDetailPage.tsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { Pokemon } from "../types";
 
-const PokemonDetailPage = () => {
+const PokemonDetailPage = ({ route }) => {
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const { id } = route.params;
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      if (id === undefined) {
+        return;
+      }
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      const data = await response.json();
+      setPokemon(data);
+    };
+    getPokemon();
+  }, [id]);
   return (
     <View>
       <Text>Pokemon Detail page</Text>
+      <Text>{ pokemon && pokemon.name}</Text>
     </View>
   );
 };
